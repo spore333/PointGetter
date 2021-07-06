c056d8abd58832291990907e853e183c1155e855
Jack Leonard <fuzhongya@gmail.com>
2021-07-06 08:30:01 -0700

    Initial Commit

---
+++ Assets/Scripts/scr_button.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_button : MonoBehaviour
+{
+    public SpriteRenderer spr;
+    public Sprite butt_up;
+    public Sprite butt_down;
+    public GameObject controller;
+    scr_controller cont;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cont = controller.GetComponent<scr_controller>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cont.score += 1;
+    }
+
+    //when clicked
+    void OnMouseDown()
+    {
+        spr.sprite = butt_down;
+        cont.score*=10;
+
+    }
+    void OnMouseUp()
+    {
+        spr.sprite = butt_up;
+    }
+    void OnMouseExit()
+    {
+        spr.sprite = butt_up;
+    }
+}   
---
+++ Assets/Scripts/scr_draw_score.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class scr_draw_score : MonoBehaviour
+{
+    public Transform t;
+    public Text txt;
+    public GameObject controller;
+    scr_controller cont;
+    public GameObject icon;
+    Transform icon_t;
+    float ico_origX;
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cont = controller.GetComponent<scr_controller>();
+        icon_t = icon.GetComponent<Transform>();
+        ico_origX = icon_t.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //int spr_width = icon.g
+        int points = cont.score;
+        txt.text = points.ToString();
+
+        float txt_w = txt.preferredWidth;
+        icon_t.position = new Vector3(ico_origX-(pixelToWorld(txt_w)/2)-0.5f, icon_t.position.y);
+    }
+
+    float pixelToWorld(float len)
+    {
+        float ret;
+        ret = cam.ScreenToWorldPoint(new Vector3((Screen.width / 2) + len, 0, 0)).x;
+        return ret;
+    }
+}
