201dd8c7f901709b793814735526803eb1481f52
jmargie <87029994+jmargie@users.noreply.github.com>
2021-07-07 17:33:21 -0400

    added crit point button
    still need sprite

--- Assets/Scripts/scr_button.cs
+++ Assets/Scripts/scr_button.cs
@@ -1,40 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; 
 
-public class scr_button : MonoBehaviour
-{
+
+public class scr_button : MonoBehaviour {
     public SpriteRenderer spr;
     public Sprite butt_up;
     public Sprite butt_down;
+    public Sprite critbutt_down;
+    public Sprite critbutt_up;
     public GameObject controller;
+    //public GameObject generator;
+    //RandomGenerator gen;
     scr_controller cont;
+    public bool special;
+    public int ran_n;
 
     // Start is called before the first frame update
     void Start()
     {
         cont = controller.GetComponent<scr_controller>();
+        //gen = generator.GetComponent<RandomGenerator>();
+        special = false;
+        ran_n = 1000;
     }
 
+    private readonly System.Random ran = new System.Random();  
+  
+    // Generates a random number within a range.      
+    public int RandomNumber(int min, int max)  
+    {  
+    	return ran.Next(min, max);  
+    }  
+
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        //cont.score += 1;
+    	if (RandomNumber(0,ran_n) == 0) {
+    		special = true;
+    		spr.sprite = critbutt_up;
+    	}
+
+    	//for testing only
+    	if (special){
+    		spr.sprite = critbutt_up;
+    	}
+
     }
 
     //when clicked
-    void OnMouseDown()
-    {
-        spr.sprite = butt_down;
-        cont.score++;
+    void OnMouseDown() {
+    	if (!special){
+	        spr.sprite = butt_down;
+	        cont.score++;
+	    }
+	    if (special){
+	    	spr.sprite = critbutt_down;
+	    	if (cont.score<50){
+	    		cont.score+=5;
+	    	}
+	    	else {
+	    		cont.score+=(int)(cont.score*.1);
+	    	}
+	    	special = false;
+	    }
 
     }
-    void OnMouseUp()
-    {
+    void OnMouseUp() {
         spr.sprite = butt_up;
     }
-    void OnMouseExit()
-    {
+    void OnMouseExit() {
         spr.sprite = butt_up;
     }
+
+
 }   
