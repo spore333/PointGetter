f7b7370dec6f04963526fa6f648a46bd9ffedf33
k8b13 <87030118+k8b13@users.noreply.github.com>
2021-07-12 10:54:14 -0400

    points and stuff

---
+++ Assets/Scripts/scr_InvButton.cs
@@ -0,0 +1,48 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scr_InvButton : MonoBehaviour
+{
+	public SpriteRenderer spr;
+    public Sprite butt_up;
+    public Sprite butt_down;
+    public GameObject controller;
+    scr_controller cont;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cont = controller.GetComponent<scr_controller>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown() {
+	    spr.sprite = butt_down;
+    }
+    void OnTouchStart() {
+        spr.sprite = butt_down;
+    }
+
+    void OnMouseUp() {
+        spr.sprite = butt_up;
+        SceneManager.LoadScene(sceneName:"farmInv", LoadSceneMode.Single);
+        //SceneManager.UnloadSceneAsync(sceneName:"Home", LoadSceneMode.Single);
+    }
+    void OnTouchEnd() {
+        spr.sprite = butt_up;
+        SceneManager.LoadScene(sceneName:"farmInv", LoadSceneMode.Single);
+        //SceneManager.UnloadSceneAsync(sceneName:"Home", single);
+    }
+
+    void OnMouseExit() {
+        spr.sprite = butt_up;
+        SceneManager.LoadScene(sceneName:"farmInv", LoadSceneMode.Single);
+    }
+}
